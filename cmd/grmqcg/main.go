64b03e5a90e5316b6ecb02c97cc7f5c6d420cc99
Vinicius A. Santos <vinicius.vas.ti@gmail.com>
2024-02-07 09:42:14 -0300

    Add elapsed time measurement to main function
    Taking 100 secs w/o concurrency

--- cmd/grmqcg/main.go
+++ cmd/grmqcg/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "github.com/viniciusvasti/golang-rabbit-mq-channel-goroutine-demo/internal/mqlistener"
+import (
+	"time"
+
+	"github.com/viniciusvasti/golang-rabbit-mq-channel-goroutine-demo/internal/mqlistener"
+)
 
 func main() {
+	startTime := time.Now()
 	rabbitMQListener := mqlistener.RabbitMQListener{}
 	rabbitMQListener.Listen()
+	elapsedTime := time.Since(startTime)
+	println("Elapsed time: ", elapsedTime / time.Second, " seconds")
 }
