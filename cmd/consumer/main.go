54def6d165779d7ae502c6b8556555ccdf5c8438
Vinicius A. Santos <vinicius.vas.ti@gmail.com>
2024-02-07 20:04:50 -0300

    Update consumer to use rabbitmq env vars To build the connection string

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/viniciusvasti/golang-rabbit-mq-channel-goroutine-demo/internal/mqlistener"
 	"github.com/viniciusvasti/golang-rabbit-mq-channel-goroutine-demo/internal/services"
 )
 
 func main() {
-	conn := services.NewRabbitMQConnection("amqp://admin:admin@rabbitmq:5672/")
+	rabbitMQHost := os.Getenv("RABBITMQ_HOST")
+	rabbitMQPort := os.Getenv("RABBITMQ_PORT")
+	rabbitMQUser := os.Getenv("RABBITMQ_USER")
+	rabbitMQPass := os.Getenv("RABBITMQ_PASS")
+	connString := fmt.Sprintf("amqp://%s:%s@%s:%s/", rabbitMQUser, rabbitMQPass, rabbitMQHost, rabbitMQPort)
+
+	conn := services.NewRabbitMQConnection(connString)
 	defer conn.Close()
 	rabbitMQListener := mqlistener.NewRabbitMQListener(conn)
 	rabbitMQListener.Listen()
