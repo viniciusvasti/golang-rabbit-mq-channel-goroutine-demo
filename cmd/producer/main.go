d6240e35b9b78eb13faa59abbd4ac7ef0cfcb990
Vinicius A. Santos <vinicius.vas.ti@gmail.com>
2024-02-07 20:10:29 -0300

    Update producer to use rabbitmq env vars from .env To build the connection string

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/viniciusvasti/golang-rabbit-mq-channel-goroutine-demo/internal/mqpublisher"
 	"github.com/viniciusvasti/golang-rabbit-mq-channel-goroutine-demo/internal/services"
+	"github.com/viniciusvasti/golang-rabbit-mq-channel-goroutine-demo/internal/util"
 )
 
 func main() {
-	conn := services.NewRabbitMQConnection("amqp://admin:admin@localhost:5672/")
+	err := godotenv.Load()
+	if err != nil {
+		util.FailOnError(err, "Error loading .env file")
+	}
+
+	rabbitMQHost := os.Getenv("RABBITMQ_HOST")
+	rabbitMQPort := os.Getenv("RABBITMQ_PORT")
+	rabbitMQUser := os.Getenv("RABBITMQ_USER")
+	rabbitMQPass := os.Getenv("RABBITMQ_PASS")
+	connString := fmt.Sprintf("amqp://%s:%s@%s:%s/", rabbitMQUser, rabbitMQPass, rabbitMQHost, rabbitMQPort)
+
+	conn := services.NewRabbitMQConnection(connString)
 	defer conn.Close()
 
 	log.Println("Publishing message to RabbitMQ")
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/viniciusvasti/golang-rabbit-mq-channel-goroutine-demo
 
 go 1.21.6
 
-require github.com/rabbitmq/amqp091-go v1.9.0 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/rabbitmq/amqp091-go v1.9.0 // indirect
+)
